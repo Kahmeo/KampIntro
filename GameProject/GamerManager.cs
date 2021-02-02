77c72e8afe0a14286a51dcbcf214befe6fa08a6a
Mehmet Ali <kahilmehmetali@gmail.com>
2021-02-02 13:59:28 +0300

    2 subat

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;  //constructor
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        //MicroService
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayıt Oldu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama Başarısız. Kayıt başarısız.");
+            }
+            
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Güncellendi");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt Silindi");
+        }
+    }
+}
